3dff7c931e047ce9140912f24576ff52cd8b7351
Jliv316 <jclivingston316@gmail.com>
2018-08-25 12:43:28 -0600

    adds routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :foods
+      resources :meals, except: [:show, :destroy] do
+        get '/foods', to: 'meals#show'
+        post '/foods/:id', to: 'meals#create'
+        delete '/foods/:id', to: 'meals#destroy'
+      end
+    end
+  end
 end
