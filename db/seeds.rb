9862ebe34b9377fd9363dce6898f223f7a13f9a0
Jliv316 <jclivingston316@gmail.com>
2018-08-25 12:45:39 -0600

    creates seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+foods = ['Chicken', 'Bread', 'Candy', 'Baby Back Ribs', 'Carrots']
+calories = [100, 250, 500, 230, 120]
+i = 0
+while i < foods.count
+  Food.create(name: foods[i], calories: calories[i])
+  i += 1
+end
+
+meals = ["Breakfast", "Snack", "Lunch", "Dinner"]
+
+meals.each do |meal|
+  Meal.create(name: meal)
+end
